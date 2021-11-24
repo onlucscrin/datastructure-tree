5b45ce59fe921f911ecf4f5e0392e759665fb28e
onlucscrin <vinutha.m.reddy@gmail.com>
2021-11-23 21:41:58 -0500

    Initial Version

---
+++ src/main/java/com/prolearning/datastructure/treedatastructure/MinimumDepthHeightTree.java
@@ -0,0 +1,7 @@
+package com.prolearning.datastructure.treedatastructure;
+
+public class MinimumDepthHeightTree {
+    public static void main(String args[]){
+        System.out.println("Here in the main...");
+    }
+}
