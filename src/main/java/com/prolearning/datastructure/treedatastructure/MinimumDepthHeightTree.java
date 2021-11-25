f9ba0bc09f71b8b71717ce8cb2d7f8163a48af40
onlucscrin <vinutha.m.reddy@gmail.com>
2021-11-25 06:27:54 -0500

    Initial Version

--- src/main/java/com/prolearning/datastructure/treedatastructure/MinimumDepthHeightTree.java
+++ src/main/java/com/prolearning/datastructure/treedatastructure/MinimumDepthHeightTree.java
@@ -1,12 +1,28 @@
 package com.prolearning.datastructure.treedatastructure;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class MinimumDepthHeightTree {
     public static void main(String args[]){
         System.out.println("Here in the main...");
+        Integer[] nodes = {3,9,20,null,null,15,7};
+        TreeNode<Integer> treeNode = TreeNode.createTree(nodes);
+        int depth = maxDepth(treeNode);
+        System.out.println("Depth : "+ depth);
+
     }
-//    public TreeNode
-//    public int maxDepth(TreeNode root) {
-//
-//    }
+
+    public static int maxDepth(TreeNode root) {
+        Deque<TreeNode> dq = new ArrayDeque<>();
+        dq.add(root);
+        int level =0;
+        while(!dq.isEmpty()){
+            TreeNode node = dq.poll();
+            dq.add(node.getLeftChild());
+            dq.add(node.getRightChild());
+            level = level+1;
+        }
+    }
 
 }
--- src/main/java/com/prolearning/datastructure/treedatastructure/TreeNode.java
+++ src/main/java/com/prolearning/datastructure/treedatastructure/TreeNode.java
@@ -10,7 +10,7 @@ import java.util.Stack;
  * @param <T>
  */
 @Data
-public class TreeNode<T> {
+public class TreeNode<T> implements Comparable {
     private T value;
     private TreeNode leftChild;
     private TreeNode rightChild;
@@ -27,33 +27,71 @@ public class TreeNode<T> {
         System.out.println("Tree Node main here");
         System.out.println("Tree : ");
         Integer[] array = {1, 2, 3, 4, 5, 6, 7};
-        TreeNode treeNode = createTreeRecursion(array, 0, 0);
-        System.out.println(treeNode);
-    }
-    public TreeNode<T> createTree(Integer[] nodes, int i){
-        TreeNode rootNode = null;
-        if(nodes.length>1)
-            rootNode = new TreeNode(nodes[0]);
+        TreeNode rootNode = createTreeRecursion(array, 0, 0);
+        System.out.println(rootNode);
+        printTree(rootNode);
 
-        PriorityQueue<TreeNode>  = new Stack();
-        for(int i=0;i<nodes.length;i++){
-            if(treeNodesStack.isEmpty())
-                treeNodesStack.push(nodes[i]);
-            else
-            {
-                treeNodesStack.pop().value = nodes[i];
-                treeNodesStack.
+    }
+
+    public static void printTree(TreeNode rootNode){
+        PriorityQueue<TreeNode<Integer>> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(rootNode);
+        while(!priorityQueue.isEmpty()){
+            TreeNode temp = priorityQueue.poll();
+            System.out.println(temp.value);
+
+            if(temp.leftChild!=null)
+                priorityQueue.add(temp.leftChild);
+            if(temp.rightChild!=null)
+                priorityQueue.add(temp.rightChild);
+        }
+    }
+
+    public static TreeNode createTree(Integer[] nodes){
+
+        if(nodes.length<1)
+            return null;
+
+        int index = 0;
+
+        PriorityQueue<TreeNode> priorityQueue = new PriorityQueue();
+        TreeNode rootNode = new TreeNode(nodes[0]);
+        priorityQueue.offer(rootNode);
+
+        while(!priorityQueue.isEmpty()){
+            TreeNode parentNode = priorityQueue.poll();
+            if (index + 1 < nodes.length ) {
+                parentNode.leftChild = new TreeNode(nodes[++index]);
+                priorityQueue.add(parentNode.leftChild);
+            }
+            if (index + 1 < nodes.length ) {
+                parentNode.rightChild = new TreeNode(nodes[++index]);
+                priorityQueue.add(parentNode.rightChild);
             }
         }
-
-
+        return rootNode;
     }
     public static TreeNode createTreeRecursion(Integer[] nodes, int i, int level){
-        if(2*level >= nodes.length)
+        int j = (((int)Math.pow(2, level))-1 ) ;
+        if(i+j >= nodes.length)
             return null;
+
+        System.out.println("i = " + i +" , j :" + j +", i+j:" + (i+j));
         TreeNode rootNode = new TreeNode(nodes[i]);
-        rootNode.setLeftChild(createTreeRecursion(nodes, (2*level+1), level+1));
-        rootNode.setRightChild(createTreeRecursion(nodes, (2*level+2) , level+1));
+        rootNode.setLeftChild(createTreeRecursion(nodes, i,level+1));
+        rootNode.setRightChild(createTreeRecursion(nodes, i+1,  level+1));
         return rootNode;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        int value1 = (int) ((TreeNode)o).value;
+        if((int)this.value == value1)
+            return 0;
+        if((int)this.value > value1)
+            return 1;
+        if((int)this.value < value1)
+            return -1;
+        return -1;
     }
 }
